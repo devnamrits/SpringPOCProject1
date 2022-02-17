4bbb8acb8c74b5c3dca385c409e574c0e538bd2b
devnamrits <hrk10321@gmail.com>
2022-02-17 16:57:42 +0530

    Added first Test Controller

---
+++ src/main/java/com/springpoc/springpocproject1/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.springpoc.springpocproject1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/home")
+    public String hello(){
+        return "Hello Spring";
+    }
+}
