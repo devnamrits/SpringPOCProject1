f36434e1f2d530f603d391ef44c4f6b3f93354d6
devnamrits <hrk10321@gmail.com>
2022-02-17 16:05:09 +0530

    Initialized Git and Project Setup

---
+++ src/main/java/com/springpoc/springpocproject1/SpringPocProject1Application.java
@@ -0,0 +1,13 @@
+package com.springpoc.springpocproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPocProject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPocProject1Application.class, args);
+    }
+
+}
